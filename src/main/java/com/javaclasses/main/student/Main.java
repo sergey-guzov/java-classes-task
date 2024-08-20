1cb887e167ac078808df148256e1abb6a1d952fd
Sergey Guzov <guzov1993@gmail.com>
2024-08-20 16:11:29 +0200

    app v1.1 (students, small improvements in output)

--- src/main/java/com/javaclasses/main/student/Main.java
+++ src/main/java/com/javaclasses/main/student/Main.java
@@ -1,22 +1,21 @@
 package com.javaclasses.main.student;
 public class Main {
     public static void main(String[] args) {
-/*       StudentCreator creator = new StudentCreator();
-       Student student1 = creator.createStudenName();
-       creator.addAddress(student1);
-       creator.addBirthday(student1);
-       creator.setFacultet(student1);
-       creator.setCource(student1);
-       creator.setGoup(student1);
-       creator.setPhoneNumber(student1);
-       System.out.println(student1.toString());
-       Student student2 = creator.createStudenName();
-       creator.addAddress(student2);
-       creator.addBirthday(student2);
-       creator.setFacultet(student2);
-       creator.setCource(student2);
-       creator.setGoup(student2);
-       creator.setPhoneNumber(student2);*/
+       StudentCreator creator = new StudentCreator();
+       Student student3 = creator.createStudenName();
+       creator.addAddress(student3);
+       creator.addBirthday(student3);
+       creator.setFacultet(student3);
+       creator.setCource(student3);
+       creator.setGoup(student3);
+       creator.setPhoneNumber(student3);
+       Student student4 = creator.createStudenName();
+       creator.addAddress(student4);
+       creator.addBirthday(student4);
+       creator.setFacultet(student4);
+       creator.setCource(student4);
+       creator.setGoup(student4);
+       creator.setPhoneNumber(student4);
        Student student1 = new Student("Sergey", "Guzov", "Eugenevich", "+375295410981",
                Facultet.UPP, (byte) 5, "UA51","05","05","1993","Zhlobin","mn16","d8","kw36");
        Student student2 = new Student("Egor", "Paloika", "Mikalaevich", "+375294559981",
@@ -24,6 +23,8 @@ public class Main {
        StudentsList list = new StudentsList();
        list.addStudent(student1);
        list.addStudent(student2);
+       list.addStudent(student3);
+       list.addStudent(student4);
        Filter filter = new Filter(list.getStudents());
        System.out.println(filter.studentsBirthday());
        System.out.println(filter.studentsOfFacultet());
